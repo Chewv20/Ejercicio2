9f0656798a40558f389712b16626e92a77ebbe15
jesus <jesusarturov20@gmail.com>
2022-05-22 22:13:34 -0500

    Creación clase QR

--- app/src/main/java/com/tallercmovil/ejercicio2/MainActivity.kt
+++ app/src/main/java/com/tallercmovil/ejercicio2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.tallercmovil.ejercicio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Click(view: View) {
+        startActivity(Intent(this, QR::class.java))
     }
 }
---
+++ app/src/main/java/com/tallercmovil/ejercicio2/QR.kt
@@ -0,0 +1,137 @@
+package com.tallercmovil.ejercicio2
+
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.Result
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+import java.net.MalformedURLException
+import java.net.URL
+
+class QR : AppCompatActivity(), ZXingScannerView.ResultHandler {
+
+    private val PERMISO_CAMARA = 1
+    private var scannerView: ZXingScannerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        scannerView = ZXingScannerView(this)
+        setContentView(scannerView)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checarPermiso()){
+                //se concedió el permiso
+            }else{
+                solicitarPermiso()
+            }
+        }
+
+        scannerView?.setResultHandler(this)
+        scannerView?.startCamera()
+    }
+
+    private fun solicitarPermiso() {
+        ActivityCompat.requestPermissions(this@QR, arrayOf(Manifest.permission.CAMERA),PERMISO_CAMARA)
+    }
+
+    private fun checarPermiso(): Boolean {
+        return (ContextCompat.checkSelfPermission(this@QR, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    override fun handleResult(p0: Result?) {
+        //código QR leído
+        val scanResult = p0?.text
+
+        Log.d("QR_LEIDO", scanResult!!)
+
+        try{
+            val url = URL(scanResult)
+            val i = Intent(Intent.ACTION_VIEW)
+            i.setData(Uri.parse(scanResult))
+            startActivity(i)
+            finish()
+        }catch(e: MalformedURLException){
+            AlertDialog.Builder(this@QR)
+                .setTitle("Error")
+                .setMessage("El código QR no es válido para la aplicación")
+                .setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.dismiss()
+                    finish()
+                })
+                .create()
+                .show()
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checarPermiso()){
+                if(scannerView == null){
+                    scannerView = ZXingScannerView(this)
+                    setContentView(scannerView)
+                }
+
+                scannerView?.setResultHandler(this)
+                scannerView?.startCamera()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView?.stopCamera()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+
+            PERMISO_CAMARA -> {
+                if(grantResults.isNotEmpty()){
+                    if(grantResults[0]!= PackageManager.PERMISSION_GRANTED){
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                                AlertDialog.Builder(this@QR)
+                                    .setTitle("Permiso requerido")
+                                    .setMessage("Se necesita acceder a la cámara para leer los códigos QR")
+                                    .setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialogInterface, i ->
+                                        requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISO_CAMARA)
+                                    })
+                                    .setNegativeButton("Cancelar", DialogInterface.OnClickListener { dialogInterface, i ->
+                                        dialogInterface.dismiss()
+                                        finish()
+                                    })
+                                    .create()
+                                    .show()
+                            }else{
+                                Toast.makeText(this@QR, "El permiso de la cámara no se ha concedido", Toast.LENGTH_LONG).show()
+                                finish()
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
